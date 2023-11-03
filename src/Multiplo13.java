081c33870c7aaf689d5322c80ebabf9cee79c19e
odracirvilo <ricardo.vilo.cgsdaw@gmail.com>
2023-11-03 10:52:17 +0100

    Ejercicio Multiplo 13 v1 Finalizado

--- src/Multiplo13.java
+++ src/Multiplo13.java
@@ -1,3 +1,4 @@
+	import java.util.Scanner;
 
 public class Multiplo13 
 
@@ -9,8 +10,25 @@ public class Multiplo13
 		 * múltiplo de 13, o no.
 		 * [v2] + si ese numero es múltiplo de X, o no. (Dónde X lo introduce el usuario)*/
 		 
-		 
-		 
+		Scanner entrada = new Scanner(System.in);
+		
+		int numeroAleatorio = 0;
+		System.out.println("Introduzca un numero aleatorio: ");
+		numeroAleatorio = entrada.nextInt();
+		double resultado = 0;
+		resultado = numeroAleatorio % 13;
+		
+		if(resultado == 0)
+		{
+			System.out.println("El numero aleatorio SI es multiplo de 13.");
+		}
+		
+		else
+		{
+			System.out.println("El numero aleatorio NO es multiplo de 13.");
+		}	
+		
+			entrada.close();
 	}
 
 }
